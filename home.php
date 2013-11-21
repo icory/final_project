a607abfd4130003a66582d25f7f65b315a69c9c0
Isaiah Cory <icory@uvm.edu>
2013-11-21 15:36:05 -0500

    Initial style

---
+++ home.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title></title>
+<meta charset="utf-8">
+<meta name="author" content="Isaiah Cory">
+<meta name="description" content="">
+    
+<link rel="stylesheet"
+href="stylesheets/foundation.css"
+media="screen">
+
+<!--[if lt IE 9]>
+    <script src="//html5shim.googlecode.com/sin/trunk/html5.js"></script>
+<![endif]-->
+    
+</head>
+
+<body id="">
+<div class="container">
+
+<div class="row navrow">
+  <nav>
+    <ul class="nav-bar">
+      <li class="activePage"><a href="">Home</a></li>
+      <li><a href="about.php">About</a></li>
+      <li><a href="stuff.php">Stuff</a></li>    
+    </ul>
+  </nav>
+</div> <!-- end row -->
+
+<div class="row window">
+  <section class="twelve columns">
+    <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+  </section>
+</div> <!-- end row -->
+
+<!-- ###########   meaningful about what this part   ################## -->
+<div class="row">
+  <section id="main" class="twelve columns">
+    <h1>main title for page</h1>
+    <p>What ever you want to say goes here</p>
+  </section>
+</div> <!-- end row -->
+<!-- ###########  search for  html5 article vs section  ################## -->
+
+<!-- ###########   is all about                      ################## -->
+<aside id="rightColumn">
+<h2>Extra info</h2>
+
+<p>Whatever you want to say that is</p>
+
+</aside>
+
+<!-- ###########                                     ################## -->
+
+<footer>
+    <p>Your name maybe</p>
+</footer>
+
+</div> <!-- end div.container --> 
+</body>
+</html>
