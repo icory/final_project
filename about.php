af534fd5b277e816d21c3d29ed27f330436e9e10
Isaiah Cory <icory@uvm.edu>
2013-11-25 23:00:55 -0500

    added about page and filled it with contents

---
+++ about.php
@@ -0,0 +1,16 @@
+<?php include('top.php'); ?>
+
+<body id="">
+
+<?php include ('header.php'); ?>
+
+<section>
+  <h1>About Us</h2>
+  <figure><img href="" alt="photo"><figcaption>Caption goes here.</figcaption></figure>
+  <p>Text here.</p>
+</section>
+
+<?php include ('footer.php'); ?>
+
+</body>
+</html>
