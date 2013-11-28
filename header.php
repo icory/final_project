634462eb35ad873a61ba1aabde93e6dead6ffa25
Isaiah Cory <icory@uvm.edu>
2013-11-27 21:15:07 -0500

    Added requestServices page

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
   <ul>
     <li><a href="home.php">Home</a></li>
     <li><a href="about.php">About</a></li>
-    <li><a href="about.php">Request Services</a></li>
+    <li><a href="requestServices.php">Request Services</a></li>
     <li><a href="contact.php">Contact</a></li>
     <li><a href="requests.php">Admin View</a></li>
   </ul>
---
+++ requestServices.php
@@ -0,0 +1,45 @@
+<?php include('top.php'); ?>
+
+<body id="">
+
+<?php include ('header.php'); ?>
+
+<section>
+  <ul>
+    <li><a href="">Translation Services</a></li>
+    <li><a href="">Interpretation Services</a></li>
+  </ul>
+</section>
+
+<section id="">
+  <h2>Translation Services</h2>
+    <ul class="">
+      <li>Service #1</li>
+      <li>Service #2</li>
+      <li>Service #3</li>
+      <li>Service #4</li>
+      <li>Service #5</li>
+    </ul>
+    <a href="">Request translation services</a>
+</section>
+
+<section id="">
+  <h2>Interpretation Services</h2>
+    <ul class="">
+      <li>Service #1</li>
+      <li>Service #2</li>
+      <li>Service #3</li>
+      <li>Service #4</li>
+      <li>Service #5</li>
+    </ul>
+    <a href="">Request interpretation services</a>
+</section>
+
+<section>
+  <p>Further information regarding these services.</p>
+<section>
+
+<?php include ('footer.php'); ?>
+
+</body>
+</html>
