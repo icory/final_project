5d3993c4b9bd5f463cec47fcb537c22523413543
Isaiah Cory <icory@uvm.edu>
2013-11-28 14:43:29 -0500

    changed h1 to h2

--- about.php
+++ about.php
@@ -5,7 +5,7 @@
 <?php include ('header.php'); ?>
 
 <section>
-  <h1>About Us</h2>
+  <h2>About Us</h2>
   <figure><img href="" alt="photo"><figcaption>Caption goes here.</figcaption></figure>
   <p>Text here.</p>
 </section>
