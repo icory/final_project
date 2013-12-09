f6c32384956b521e9887dfc9c5b5c9506f61a7f0
Isaiah Cory <icory@uvm.edu>
2013-12-09 17:53:33 -0500

    structural change of the divs

--- requestServices.php
+++ requestServices.php
@@ -4,16 +4,11 @@
 
 <?php include ('header.php'); ?>
 
-<section>
-  <ul>
-    <li><a href="requestTranslation.php">Translation Services</a></li>
-    <li><a href="requestInterpretation.php">Interpretation Services</a></li>
-  </ul>
-</section>
-
-<section id="">
+<div class="container">
+<div class="row">
+<section class="col six">
   <h2>Translation Services</h2>
-    <ul class="">
+    <ul class="service-list">
       <li>Detail #1</li>
       <li>Detail #2</li>
       <li>Detail #3</li>
@@ -23,9 +18,9 @@
     <a href="requestTranslation.php">Request a free quote for translation services</a>
 </section>
 
-<section id="">
+<section class="col six">
   <h2>Interpretation Services</h2>
-    <ul class="">
+    <ul class="service-list">
       <li>Detail #1</li>
       <li>Detail #2</li>
       <li>Detail #3</li>
@@ -34,10 +29,15 @@
     </ul>
     <a href="requestInterpretation.php">Request interpretation services</a>
 </section>
+    <br><br>
+</div> <!-- end div.row -->
 
+<div class="row">
 <section>
   <p>Further information regarding these services.</p>
 <section>
+</div> <!-- end div.row -->
+</div> <!-- end container -->
 
 <?php include ('footer.php'); ?>
 
