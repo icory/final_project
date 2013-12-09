656a525bc2137e1aba5f0a2333796d6e5d93448b
Isaiah Cory <icory@uvm.edu>
2013-12-09 17:52:44 -0500

    added photo and lorem ipsum text

--- about.php
+++ about.php
@@ -4,11 +4,15 @@
 
 <?php include ('header.php'); ?>
 
-<section>
+<div class="container">
+<div class="row">
+<section class="twelve col">
   <h2>About Us</h2>
-  <figure><img href="" alt="photo"><figcaption>Caption goes here.</figcaption></figure>
-  <p>Text here.</p>
+  <figure><img src="imgdir/who-we-are.jpg" class="flex lfloat" alt="photo" width="450px"></figure>
+  <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
 </section>
+</div> <!-- end div.row -->
+</div> <!-- end div.container -->
 
 <?php include ('footer.php'); ?>
 
