91fc31a1c608851b4e4da13cbbf56f22cb6ca33d
Isaiah Cory <icory@uvm.edu>
2013-12-11 01:49:24 -0500

    added div.row and div.col

--- header.php
+++ header.php
@@ -1,16 +1,31 @@
-<img src="" alt="logo">
+<div class="row">
+ <div class="twelve col branding">
+  <img src="imgdir/logo.png" alt="logo" class="logo">
+ </div>
+</div>
 
-<nav>
-  <ul>
+<div class="row">
+ <div class="twelve col">
+  <nav>
+   <ul>
     <li><a href="home.php">Home</a></li>
     <li><a href="about.php">About</a></li>
     <li><a href="requestServices.php">Request Services</a></li>
     <li><a href="contact.php">Contact</a></li>
     <li><a href="adminView.php">Admin View</a></li>
-  </ul>
-</nav>
+   </ul>
+  </nav>
+ </div>
+</div>
 
-<ul>
-  <li>(+1)XXX-XXX-XXXX</li>
-  <li>info@company.org</li>
-</ul>
+
+<div class="row">
+ <div class="twelve col companycontact"><br/>
+<!--
+  <ul>
+   <li>(+1)XXX-XXX-XXXX</li>
+   <li>info@company.org</li>
+  </ul>
+-->
+ </div>
+</div>
