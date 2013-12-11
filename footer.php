2ed29bee1d7ef4701945d620739300b04361aa9b
Isaiah Cory <icory@uvm.edu>
2013-12-11 01:48:40 -0500

    added div.row and div.col

--- footer.php
+++ footer.php
@@ -1,3 +1,7 @@
-<footer>
-  <p>Professional web design by Isaiah Cory</p>
-</footer>
+<div class="row">
+ <div class="twelve col clearfix">
+  <footer>
+   <p>Professional web design by Isaiah Cory</p>
+  </footer>
+ </div>
+</div>
